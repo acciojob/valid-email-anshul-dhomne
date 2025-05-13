fdc6338fab25aeb39da475351ece6f4a33e26c61
anshul-dhomne <anshul.dhomnee@gmail.com>
2025-05-13 15:11:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function validEmail(str) {
-  //your JS code here.
-}
+function validEmail(str: string): boolean {
+  // Return false if the input is empty
+  if (!str) return false;
 
-// Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+  // Regular expression for validating email
+  const emailRegex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
+
+  return emailRegex.test(str);
+}
